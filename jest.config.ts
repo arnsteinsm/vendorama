7a7fe7e0e6eda792033e9f52c2f272f62e1ac195
Arnstein Snyen Marjala <arnstein@giof.se>
2024-05-13 11:59:01 +0200

    init

--- jest.config.ts
+++ jest.config.ts
@@ -5,5 +5,19 @@ const config: Config.InitialOptions = {
   testEnvironment: "node",
   verbose: true,
   //automock: true,
+  coverageDirectory: "coverage",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/**/index.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  clearMocks: true,
+  resetModules: true,
 };
 export default config;
--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./bringClient";
-export * from "./cleanAndSplitProductNames";
-export * from "./mapboxClient";
-export * from "./productOperations";
-export * from "./sanityClient";
-export * from "./sanityOperations";
-export * from "./slugifyString";
